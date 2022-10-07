c848c8b00272de2c8a048f3e9a7fa6817bafb51a
MartinHoltsmeier <martinholtsmeier@gmail.com>
2022-10-07 19:33:41 +0300

    More clean code.

--- 4_Tallinn_University_Week_4/data.php
+++ 4_Tallinn_University_Week_4/data.php
@@ -1,13 +1,13 @@
 <?php
-	//ühendan sessiooniga
+	// Ühendan sessiooniga.
 	require("functions.php");
 
-	//kui ei ole sisseloginud, suunan login lehele
+	// Kui ei ole sisseloginud, suunan login lehele.
 	if (!isset($_SESSION["userId"])) {
 		header("Location: login.php");
 	}
-	//kas aadressireal on logout
-	
+
+	// Kas aadressireal on logout?
 	if (isset($_GET["logout"])) {
 		
 		session_destroy();
@@ -16,10 +16,9 @@
 	}
 
 ?>
-<h1>Data</h1>
 
+<h1>Data</h1>
 <p>
 	Tere tulemast <?=$_SESSION["userEmail"];?>!
-	<a href="?logout=1">logi välja</a>
-
+	<a href="?logout=1">logi välja</a> <!-- Lisab ?logout=1 aadressi reale log -->
 </p>
--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $conn = new mysqli($serverHost, $serverUsername, $serverPassword);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $conn->connect_error);
 }
 
 // Create database.
@@ -17,9 +17,9 @@ $sql = "CREATE DATABASE myDB";
 
 // Check if successfully created database.
 if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
+	echo "Database created successfully";
 } else {
-  echo "Error creating database: " . $conn->error;
+	echo "Error creating database: " . $conn->error;
 }
 
 // Close connection.
@@ -30,21 +30,21 @@ $conn = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 
 // Check connection.
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $conn->connect_error);
 }
 
 // Sql to create table.
 $sql = "CREATE TABLE user_sample (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(50) NOT NULL,
-  password VARCHAR(128) NOT NULL
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(50) NOT NULL,
+	password VARCHAR(128) NOT NULL
 )";
 
 // Check if successfully created table.
 if ($conn->query($sql) === TRUE) {
-  echo "Table MyGuests created successfully";
+	echo "Table MyGuests created successfully";
 } else {
-  echo "Error creating table: " . $conn->error;
+	echo "Error creating table: " . $conn->error;
 }
 
 // Close connection.
